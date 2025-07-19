8ef23f9f48d3baed4df2b683dfb780a56bac5371
Zenitsu Agatsuma <169963839+zenitsua2416@users.noreply.github.com>
2025-07-20 00:19:27 +0530

    [BA-1]: Some more config for prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,10 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-import simpleImportSort from "eslint-plugin-simple-import-sort";
-import react from "eslint-plugin-react";
-import reactHooks from "eslint-plugin-react-hooks";
-import jsxA11y from "eslint-plugin-jsx-a11y";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import jsxA11y from 'eslint-plugin-jsx-a11y';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -15,38 +15,38 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends(
-    "next/core-web-vitals",
-    "next/typescript",
-    "plugin:react/recommended",
-    "plugin:react-hooks/recommended",
-    "plugin:jsx-a11y/recommended",
+    'next/core-web-vitals',
+    'next/typescript',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
   ),
   {
     plugins: {
-      "simple-import-sort": simpleImportSort,
+      'simple-import-sort': simpleImportSort,
       react,
-      "react-hooks": reactHooks,
-      "jsx-a11y": jsxA11y,
+      'react-hooks': reactHooks,
+      'jsx-a11y': jsxA11y,
     },
     rules: {
-      "simple-import-sort/imports": [
-        "error",
+      'simple-import-sort/imports': [
+        'error',
         {
           groups: [
-            ["^react", "^react-dom", "^react-hook-form"],
-            ["^next", "^next/.*"],
-            ["^@/"],
-            ["^\\.", "^\\..*"],
+            ['^react', '^react-dom', '^react-hook-form'],
+            ['^next', '^next/.*'],
+            ['^@/'],
+            ['^\\.', '^\\..*'],
           ],
         },
       ],
-      "react/prop-types": "off",
-      "react/react-in-jsx-scope": "off",
-      "jsx-a11y/anchor-is-valid": "off",
+      'react/prop-types': 'off',
+      'react/react-in-jsx-scope': 'off',
+      'jsx-a11y/anchor-is-valid': 'off',
     },
     settings: {
       react: {
-        version: "detect",
+        version: 'detect',
       },
     },
   },
