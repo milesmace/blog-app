40d29ab59c9d0780a927984df39b0accbb7da4c8
Miles Mace <169963839+milesmace@users.noreply.github.com>
2025-08-03 17:26:01 +0530

    perf: Remove unwanted function usage

--- src/components/icons/Spinner/Spinner.component.tsx
+++ src/components/icons/Spinner/Spinner.component.tsx
@@ -1,7 +1,5 @@
 import { Loader } from 'lucide-react';
 
-import { cn } from '@/lib';
-
 export const Spinner = ({ ...props }: React.ComponentProps<'svg'>) => {
-  return <Loader {...props} className={cn('animate-spin')} />;
+  return <Loader {...props} className="animate-spin" />;
 };
