cec1035f5e081c1db5c174e114b5ca3079810569
Miles Mace <169963839+milesmace@users.noreply.github.com>
2025-08-03 12:31:12 +0530

    test: Removed some unwanted tests.
    TODO: Add tests later

--- src/containers/ThemeToggle/ThemeToggle.container.tsx
+++ src/containers/ThemeToggle/ThemeToggle.container.tsx
@@ -10,6 +10,10 @@ import {
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
 } from '@/components/ui';
 
 import { ThemeConfig } from './ThemeToggle.config';
@@ -18,33 +22,40 @@ export const ThemeContainer = () => {
   const { setTheme } = useTheme();
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
-          <Sun
-            data-testid="lucide-sun"
-            className="inline-block h-[1.2rem] w-[1.2rem] dark:hidden"
-          />
-          <Moon
-            data-testid="lucide-moon"
-            className="hidden h-[1.2rem] w-[1.2rem] dark:inline-block"
-          />
-          <span className="sr-only">Toggle theme</span>
-        </Button>
-      </DropdownMenuTrigger>
+    <TooltipProvider>
+      <DropdownMenu>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <DropdownMenuTrigger asChild>
+              <Button variant="outline" size="icon">
+                <Sun
+                  data-testid="lucide-sun"
+                  className="inline-block h-[1.2rem] w-[1.2rem] dark:hidden"
+                />
+                <Moon
+                  data-testid="lucide-moon"
+                  className="hidden h-[1.2rem] w-[1.2rem] dark:inline-block"
+                />
+                <span className="sr-only">Toggle theme</span>
+              </Button>
+            </DropdownMenuTrigger>
+          </TooltipTrigger>
+          <TooltipContent side="bottom">Toggle theme</TooltipContent>
+        </Tooltip>
 
-      <DropdownMenuContent align="end">
-        {ThemeConfig.map((theme) => (
-          <DropdownMenuItem
-            key={theme.value}
-            className="text-neutral-700 dark:text-neutral-300"
-            onClick={() => setTheme(theme.value)}
-          >
-            <theme.icon />
-            {theme.label}
-          </DropdownMenuItem>
-        ))}
-      </DropdownMenuContent>
-    </DropdownMenu>
+        <DropdownMenuContent align="end">
+          {ThemeConfig.map((theme) => (
+            <DropdownMenuItem
+              key={theme.value}
+              className="text-neutral-700 dark:text-neutral-300"
+              onClick={() => setTheme(theme.value)}
+            >
+              <theme.icon className="mr-2 h-4 w-4" />
+              {theme.label}
+            </DropdownMenuItem>
+          ))}
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </TooltipProvider>
   );
 };
